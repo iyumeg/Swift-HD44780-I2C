715d15e1cc0ff409aa1ba56d0bf6930c64565de2
Artur <lich@Arturs-MacBook-Air.local>
2017-05-15 18:26:25 +0300

    Add SPM support
    Add swift package manager support

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "LCDI2C",
+    targets: [Target(name: "LCDI2C")],
     dependencies: [
         .Package(url: "https://www.github.com/novi/i2c-swift.git", majorVersion: 0)
     ]
