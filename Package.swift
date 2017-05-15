955bf363fc4db213a09e7f59592c3862685a6e1d
Artur <lich@Arturs-MacBook-Air.local>
2017-05-15 18:15:00 +0300

    Hot fix
     Removed an unnecessary comma

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "LCDI2C",
     dependencies: [
-        .Package(url: "https://www.github.com/novi/i2c-swift.git", majorVersion: 0),
+        .Package(url: "https://www.github.com/novi/i2c-swift.git", majorVersion: 0)
     ]
 )
